70ccdbf23b7c8773ed6c6815e3a194798c575f64
Anton <Realkom55a@Yandex.ru>
2022-10-19 23:54:37 +0300

    Коммит 012

--- HalloMark/Program.cs
+++ HalloMark/Program.cs
@@ -1,12 +1,18 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите имя пользователя: ");
+        string username = Console.ReadLine();
 
-if(username.ToLower() == "марк")
-{
-    Console.WriteLine("Ура, это же МАРК!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);  
+        if (username.ToLower() == "марк")
+        {
+            Console.WriteLine("Ура, это же МАРК!");
+        }
+        else
+        {
+            Console.Write("Привет, ");
+            Console.WriteLine(username);
+        }
+    }
 }
