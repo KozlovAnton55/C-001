cc847c79919f358f12cb6607d30eea4c8fcaa118
Anton <Realkom55a@Yandex.ru>
2022-10-20 00:25:17 +0300

    Коммит 014

--- Base/Program.cs
+++ Base/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 7;
+        int c = 9;
+        int d = 18;
+        int e = 6;
+        int max = a;
+
+        if (a > max) max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+        if (e > max) max = e;
+
+        Console.Write("max = ");
+        Console.WriteLine(max);
+    }
+}
